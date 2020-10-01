a97edf11d880555c7f487da419f330e909cb366f
Samuel Denton <samdenton1995@hotmail.co.uk>
2020-10-01 13:37:19 +0000

    Learning how to push data into a new table

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-function getData(cb) {
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+  xhr.open("GET", baseURL + type + "/");
   xhr.send();
 
   xhr.onreadystatechange = function () {
@@ -11,8 +13,32 @@ function getData(cb) {
   };
 }
 
-function printDataToConsole(data){
-console.log(data);
+function getTableHeaders(obj) {
+  var tableHeaders = [];
+
+  Object.keys(obj).forEach(function(key) {
+    tableHeaders.push(`<td>${key}</td>`);
+  });
+
+  return `<tr>${tableHeaders}</tr>`;
 }
 
-getData(printDataToConsole);
+function writeToDocument(type) {
+
+    var tableRows = [];
+  var el = document.getElementById("data");
+  el.innerHTML = "";
+  getData(type, function (data) {
+    data = data.results;
+    var tableHeaders = getTableHeaders(data[0]);
+    data.forEach(function(item) {
+        var dataRow = [];
+
+        Object.keys(item).forEach(function(key){
+            dataRow.push(`<td>${item[key]}</td>`);
+        })
+      tableRows.push(dataRow);
+    });
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+  });
+}
