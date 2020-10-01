71bbdee6b116fcb29a0983f79037f8bcc6c8efda
Samuel Denton <samdenton1995@hotmail.co.uk>
2020-10-01 14:53:38 +0000

    Implemented pagination

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-function getData(type, cb) {
+
+
+ //----------------------------- Get Data Function ----------------------------------//
+function getData(url, cb) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", baseURL + type + "/");
+  xhr.open("GET", url + "/");
   xhr.send();
 
   xhr.onreadystatechange = function () {
@@ -13,34 +15,70 @@ function getData(type, cb) {
   };
 }
 
+//----------------------------- Get Data Function ----------------------------------//
+
+
+
+//----------------------------- Get Table Headers Function ----------------------------------//
+
 function getTableHeaders(obj) {
   var tableHeaders = [];
 
-  Object.keys(obj).forEach(function(key) {
+  Object.keys(obj).forEach(function (key) {
     tableHeaders.push(`<td>${key}</td>`);
   });
 
   return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
+//----------------------------- Get Table Headers Function ----------------------------------//
 
-    var tableRows = [];
+
+
+//----------------------------- Pagination Function ----------------------------------//
+
+function generatePaginationButtons(next, prev){
+    if (next && prev){
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+               `<button onclick="writeToDocument('${next}')">Next</button>`;
+    }
+    else if (next && !prev){
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    }else if (!next && prev){
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+//----------------------------- Pagination Function ----------------------------------//
+
+
+
+//----------------------------- Write Document Function ----------------------------------//
+
+function writeToDocument(url) {
+  var tableRows = [];
   var el = document.getElementById("data");
   el.innerHTML = "";
-  getData(type, function (data) {
+
+  getData(url, function (data) {
+      var pagination;
+      if (data.next || data.previous){
+          pagination = generatePaginationButtons(data.next, data.previous);
+      }
     data = data.results;
     var tableHeaders = getTableHeaders(data[0]);
-    data.forEach(function(item) {
-        var dataRow = [];
+    data.forEach(function (item) {
+      var dataRow = [];
 
-        Object.keys(item).forEach(function(key){
-            var rowData = item[key].toString();
-            var truncatedData = rowData.substring(0,15);
-            dataRow.push(`<td>${truncatedData}</td>`);
-        })
+      Object.keys(item).forEach(function (key) {
+        var rowData = item[key].toString();
+        var truncatedData = rowData.substring(0, 15);
+        dataRow.push(`<td>${truncatedData}</td>`);
+      });
       tableRows.push(`<tr>${dataRow}</tr>`);
     });
-    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
   });
 }
+
+//----------------------------- Write Document Function ----------------------------------//
